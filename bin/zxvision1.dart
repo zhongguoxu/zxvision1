f598703135befeeb2071046c346467330bc050de
zhongguoxu <zhongguo@ualberta.ca>
2023-03-06 17:05:09 -0700

    create new flutter project

---
+++ bin/zxvision1.dart
@@ -0,0 +1,5 @@
+import 'package:zxvision1/zxvision1.dart' as zxvision1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${zxvision1.calculate()}!');
+}
---
+++ test/zxvision1_test.dart
@@ -0,0 +1,8 @@
+import 'package:zxvision1/zxvision1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
